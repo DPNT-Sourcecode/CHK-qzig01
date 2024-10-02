d2c5fd2bac1c690bc4badf255a8d1dae2e7de0d3
sbx_user1051 <sbx_user1051@169.254.108.213>
2024-10-02 12:19:56 +0000

    Wed Oct 02 12:19:56 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
@@ -6,15 +6,20 @@ from typing import List
 class LineItem:
     item: str
     price: int
-    special_offer: str
+    special_offer: str = ""
+
+
+@dataclass
+class PriceTable:
+    line_items: List[LineItem] = field(default_factory=list)
 
 
 def load_price_table() -> List[LineItem]:
     return [
         LineItem(item="A", price=50, special_offer="3A for 130"),
-        LineItem(item="A", price=50, special_offer="3A for 130"),
-        LineItem(item="A", price=50, special_offer="3A for 130"),
-        LineItem(item="A", price=50, special_offer="3A for 130"),
+        LineItem(item="B", price=30, special_offer="2B for 45"),
+        LineItem(item="C", price=20),
+        LineItem(item="D", price=15),
     ]
 
 
@@ -22,4 +27,5 @@ def load_price_table() -> List[LineItem]:
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,1 +1,8 @@
-from lib.solutions.CHK.checkout_solution import checkout
+from lib.solutions.CHK.checkout_solution import load_price_table
+
+
+class TestChk:
+    def test_load_price_table_ok(self):
+        table = load_price_table()
+        assert len(table) == 4
+
