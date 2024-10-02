35fc520c3353b628209f289dbf249916d68fc4ac
sbx_user1051 <sbx_user1051@169.254.72.237>
2024-10-02 16:03:37 +0000

    Wed Oct 02 16:03:37 UTC 2024

---
+++ test/solution_tests/CHK/test_load.py
@@ -0,0 +1,7 @@
+from lib.solutions.CHK.price_table import load_from_pipe_sep
+
+
+class TestLoad:
+    def test_load_ok(self):
+        pt = load_from_pipe_sep()
+        assert pt.count_items == 20
